81d8c11eeb033e8a4f5bd78c2e908d474b1ef1e3
Я <Leshechka@leshas-imac.local>
2016-02-08 20:15:33 +0300

    Initial Commit

---
+++ abzac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  abzac
+//
+//  Created by Lesha Melnikov on 08.02.16.
+//  Copyright © 2016 Lesha Melnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
